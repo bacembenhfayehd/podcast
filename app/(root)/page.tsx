808ecf119489f54167ea8f211eac37aa32b8a3fb
Bacem <bacem.benhfayedh@edu.isetcom.tn>
2025-01-23 16:09:30 +0100

    convex setup

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,14 +1,21 @@
+"use client"
 import PodcastDetails from '@/components/PodcastDetails'
-import { Button } from '@/components/ui/button'
 import { podcastData } from '@/constants'
 import React from 'react'
+import { useQuery } from "convex/react";
+import { api } from "@/convex/_generated/api";
+
 
 const Home = () => {
+  const tasks = useQuery(api.tasks.get);
   return (
     <div className='mt-9 flex flex-col gap-9'>
       <section className='flex flex-col gap-5'>
         <h1 className='text-20 font-bold text-white-1'>Trending podacasts</h1>
-      {/*<Button variant="outline" className='bg-orange-1'>ADD</Button>*/}
+        <div className="flex min-h-screen flex-col items-center justify-between text-white-1 p-24">
+       {tasks?.map(({ _id, text }) => <div key={_id}>{text}</div>)}
+       </div>
+    
       <div className='podcast_grid'>
       {podcastData.map(({id,title,description,imgURL}) => (
         <PodcastDetails key={id} podcastId={id} imgURL={imgURL} title={title} description={description}/>
--- components/PodcastDetails.tsx
+++ components/PodcastDetails.tsx
@@ -7,7 +7,7 @@ const PodcastDetails = ({
   imgURL,
   podcastId,
 }: {
-  imgURL: string;
+  imgURL: any;
   title: string;
   description: string;
   podcastId: number;
@@ -15,7 +15,7 @@ const PodcastDetails = ({
   return (
     <div className="cursor-pointer">
         <figure className="flex flex-col gap-2">
-            <Image src={imgURL} width={174} height={174} alt={title} />
+            <Image src={imgURL} width={174} height={174} alt={title} className="aspect-square h-fit w-full rounded-2xl 2xl:size-[200px]" />
             <div className="flex flex-col">
                 <h1 className="text-16 font-bold text-white-1 truncate">{title}</h1>
                 <h2 className=" text-12 font-normal capitalize truncate text-white-4">{description}</h2>
